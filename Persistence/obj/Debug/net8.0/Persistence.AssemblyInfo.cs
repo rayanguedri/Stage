0e4dec18e960f6eb7b986934d3e84519d78651ef
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2024-03-15 17:07:32 +0100

    Added Crud , tokens and optimization

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de74dad7b8a0c01e33d3780fcbf326f9e860e2a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d03a611a51d9523cc85e0cbf8a0a5f64dd2cfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
