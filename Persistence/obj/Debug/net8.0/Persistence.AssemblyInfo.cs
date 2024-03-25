d8372e22ef457ddbd35a5bc85a6daf207281dd44
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2024-03-25 13:18:15 +0100

    improved UI

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6dfa1c49a6f3571fb78a5f7a5137fd922da9589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3353f9c9ce630a232565d3532e8d5fb1cf01f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
